7526eed26db1fc852c9057ba97ff1fe1d8c493db
kace <kace42205@gmail.com>
2018-02-14 13:07:38 -0800

    Adding red highlight for lines that are <80 cols.

--- bash_on_windows_setup.sh
+++ bash_on_windows_setup.sh
@@ -2,6 +2,7 @@
 
 # bash_on_windows_setup.sh - customizes a default instance of bash on windows
 
+### .bashrc
 # bash coloring, python startup, default editor
 # PS1 -> https://wiki.ubuntuusers.de/Bash/Prompt/
 # use on Ubuntu (dir coloring needs to be explicit) -> LS_COLORS='ow=01;36:di=01;36'
@@ -16,8 +17,11 @@ export VISUAL=vim
 export EDITOR="$VISUAL"
 " >> ~/.bashrc
 
+### .vimrc
 echo ":set background=dark
 :filetype plugin indent on
 :set expandtab
 :set tabstop=4
-:set shiftwidth=4" >> ~/.vimrc
+:set shiftwidth=4
+:highlight OverLength ctermbg=red ctermfg=white guibg=#592929
+:match OverLength /\%81v.\+/" >> /$HOME/.vimrc

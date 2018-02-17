430d25df263f83820965633190d01dc377337af2
kace <kace@gmail.com>
2018-02-17 02:59:10 -0500

    Changing .vimrc from append to overwrite. Its more convenient this way.

--- bash_on_windows_setup.sh
+++ bash_on_windows_setup.sh
@@ -24,4 +24,4 @@ echo ":set background=dark
 :set tabstop=4
 :set shiftwidth=4
 :highlight OverLength ctermbg=red ctermfg=white guibg=#592929
-:match OverLength /\%81v.\+/" >> /$HOME/.vimrc
+:match OverLength /\%81v.\+/" > /$HOME/.vimrc
